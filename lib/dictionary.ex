f54840190c29c853aa401bf4d988c0ae2b879826
Sergey Makarichev <harleymack@mail.ru>
2022-12-03 13:15:25 +0300

    small fixes

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,4 +1,10 @@
 defmodule Dictionary do
+  @list_words "assets/words.txt"
+              |> File.read!()
+              |> String.split(~r/\n/, trim: true)
+
+  @list1 2
+
   @moduledoc """
   Documentation for `Dictionary`.
   """
@@ -13,6 +19,22 @@ defmodule Dictionary do
 
   """
   def hello do
-    IO.puts("Hello world")
+    IO.puts("Hello world!!!!")
+  end
+
+  def random_word do
+    @list_words
+    |> Enum.random()
+  end
+
+  def split_test do
+    IO.puts("#{@list1}")
+    coy = "had we but world enough, and time"
+    String.split(coy, ",")
+    String.to_charlist(coy)
+    String.reverse(coy)
+    silly = "had we but bacon enough, and treacle"
+    String.myers_difference(coy, silly)
+    String.codepoints(coy)
   end
 end
